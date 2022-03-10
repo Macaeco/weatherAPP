6c3c319c958eb8e268350046a8445dcd8afacb02
LAPTOP-60CIESMU\AN515 <pepemmbesalduch@gmail.com>
2022-03-10 09:03:12 +0100

    pruebas

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,7 +2,19 @@
 import './App.css';
 import WeatherFetch from './components/weather-fetch';
 
+
+
+
 function App() {
+
+
+
+
+
+
+
+
+
   return (
     <WeatherFetch></WeatherFetch>
   );
--- weather-app/src/components/weather-fetch/index.jsx
+++ weather-app/src/components/weather-fetch/index.jsx
@@ -5,11 +5,11 @@ function WeatherFetch(){
     const [data, updateData]=useState([])
 
     useEffect(() => {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e659f9e2b322c20d032f79bdcd034fa4')
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0819690359d369d6da230a4ab8652a93')
         .then (j => j.json())
         .then (data =>updateData(data))
     }, [])
-    // console.log(data)
+    console.log(data)
 
     return(
         <p>{JSON.stringify(data)}</p>
