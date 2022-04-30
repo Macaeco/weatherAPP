4d0407bc214b53a4506a60bff4f3464ca6bcacad
macaeco <macarenafernandezayusob@gmail.com>
2022-04-30 11:07:13 +0200

    favico

--- weather-app/src/api/index.jsx
+++ weather-app/src/api/index.jsx
@@ -18,7 +18,7 @@ export const getLatAndLonFromCity = async (citySearch) => {
 export const getWeatherByLocation = async (lat, lon, unit) => {
 
 
-     let key = 'da66c2d9d69cd9be8dc442074e110412'; 
+     let key = process.env.REACT_API_KEY; 
 
      
 
@@ -31,7 +31,7 @@ export const getWeatherByLocation = async (lat, lon, unit) => {
 
 export const getCityName = async (lat, lon) => {
 
-    let key = 'da66c2d9d69cd9be8dc442074e110412'; 
+    let key = process.env.REACT_API_KEY; 
 
     const r = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
     const d = await r.json()
